f58135e4dfc8911aac1a3a571b56a27084375a0c
Gerardo-Eberh <123392749+Gerardo-Eberh@users.noreply.github.com>
2023-01-24 19:53:33 -0300

    Delete js directory

--- js/contacto.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById("boton_enviar").onclick =function(){
-    Enviar();
-};
-
-function Enviar(){
-    window.alert("El Mensaje Fue enviado Correctamente, Gracias por contactarse!!!.");
-    document.getElementById("boton_enviar").style.display= "none"
-}
